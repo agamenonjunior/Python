5b7b0a2d26f9094c8e073b0ec73a7c669a84a5b6
agamenon <agamenon_junior1@hotmail.com>
2024-05-30 18:05:45 -0300

    adição do script de Strings

---
+++ Modificar_Strings.py
@@ -0,0 +1,9 @@
+#Modificando Strings
+
+"""
+    método format() coloca na string 
+    onde estão os espaços reservados {}
+"""
+posicao = 'nova'
+txt = "A fabrica de {}"
+print(txt.format(posicao))
