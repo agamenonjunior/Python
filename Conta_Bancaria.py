15bf0d352d506a8a422d2bcbb076173cde54dc87
agamenon <agamenon_junior1@hotmail.com>
2024-06-14 22:17:58 -0300

    Refatorando a Classe Conta_Bancaria

--- Conta_Bancaria.py
+++ Conta_Bancaria.py
@@ -1,30 +1,38 @@
 class Conta:
+    #Construtor da Classe
     def __init__(self,numero,titular,saldo = 0, saldo_devedor = 0):
         self.numero = numero
         self.titular = titular
         self.saldo = saldo
         self.saldo_devedor = saldo_devedor
-        
+     
+    #Realiza o Saque da conta do usuário
+     
     def debitar(self, valor):
         if valor > self.saldo:
             print("Saldo insuficiente")
         else:
             self.saldo -=valor
+    
+    #Realiza deposito na conta do usuário
+    
     def creditar(self, valor):
         self.saldo += valor
-
+    #Retorna o saldo da conta
     def extrair_saldo(self):
         return self.saldo
-    
+    #Retorna o saldo devedor do usuário
     def extrair_saldo_devedor(self):
         return self.saldo_devedor
-    
+
+    #Realiza um acrescimo na conta do usuário e incrementa o saldo Devedor 
     def emprestimo(self, valor):
         self.saldo_devedor =  valor + (valor * 0.2)
         self.creditar(valor)
-        
+    #Realiza a dedução do saldo devedor do usuário    
     def pagar_saldo_devedor(self, valor):
         self.saldo_devedor -= valor
+    #Faz um estimativa de retorno sobre valor investido
     def investimento(self, valor,taxa, tempo_em_meses):
         retorno = valor
         for x in range (tempo_em_meses):
