6981ecabac27ba510ce7d235d1760ff8c95273af
agamenon <agamenon_junior1@hotmail.com>
2024-08-01 21:45:48 -0300

    Refatorando Algoritmo de esteganografia

--- Esteganografia.py
+++ Esteganografia.py
@@ -2,10 +2,10 @@ from stegano import lsb
 
 imagem = './imagem.png'
 mensagem = 'Oi tudo bom ?'
-
+#realiza a Esteganografia
 secreto = lsb.hide('./'+imagem,mensagem)
 secreto.save('./secreto.png')
-
+#Exibe a mensagem escondida na imagem
 reverter = lsb.reveal("./secreto.png")
 print(reverter)
 
